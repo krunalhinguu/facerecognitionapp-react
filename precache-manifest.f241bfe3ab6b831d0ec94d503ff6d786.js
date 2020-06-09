c09752d9c6ccd5b37a9cddb68e6817524d3a7b5d
krunal hingu <krunal.hingu222@gmail.com>
2020-06-09 16:40:23 +0530

    Updates

---
+++ precache-manifest.f241bfe3ab6b831d0ec94d503ff6d786.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d94370d81d10d62caf4655f658577b6",
+    "url": "/facerecognitionapp-react/index.html"
+  },
+  {
+    "revision": "791739c5ee081a4fbce6",
+    "url": "/facerecognitionapp-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6792dd5dd134952cf16d",
+    "url": "/facerecognitionapp-react/static/css/main.819b28fe.chunk.css"
+  },
+  {
+    "revision": "791739c5ee081a4fbce6",
+    "url": "/facerecognitionapp-react/static/js/2.b467d5d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognitionapp-react/static/js/2.b467d5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6792dd5dd134952cf16d",
+    "url": "/facerecognitionapp-react/static/js/main.08d39b20.chunk.js"
+  },
+  {
+    "revision": "09fa76177a5a4b3aa585",
+    "url": "/facerecognitionapp-react/static/js/runtime-main.fd0825f6.js"
+  }
+]);
