70b3bdfffce00abd758b6cd9758842e47320da21
krunal hingu <krunal.hingu222@gmail.com>
2020-06-13 01:39:37 +0530

    changed server url

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://radiant-hollows-60543.herokuapp.com/imageurl", {
       method: "post",
       headers: { "content-Type": "application/json" },
       body: JSON.stringify({
@@ -89,7 +89,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://radiant-hollows-60543.herokuapp.com/image", {
             method: "put",
             headers: { "content-Type": "application/json" },
             body: JSON.stringify({
--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("http://localhost:3000/signin", {
+    fetch("https://radiant-hollows-60543.herokuapp.com/signin", {
       method: "post",
       headers: { "content-Type": "application/json" },
       body: JSON.stringify({
